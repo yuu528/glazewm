e26eaa0cf006c6e2fae60cb279287bb90e0ebcd7
Lars Berger <lars.berger@yahoo.com>
2023-01-16 23:42:04 +0800

    feat(infra): add `IsWindow` to extern windows apis

--- GlazeWM.Infrastructure/WindowsApi/WindowsApiService.cs
+++ GlazeWM.Infrastructure/WindowsApi/WindowsApiService.cs
@@ -156,6 +156,10 @@ namespace GlazeWM.Infrastructure.WindowsApi
 
     [DllImport("user32.dll")]
     [return: MarshalAs(UnmanagedType.Bool)]
+    public static extern bool IsWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool IsWindowVisible(IntPtr hWnd);
 
     [DllImport("user32.dll", EntryPoint = "GetWindowLong")]
