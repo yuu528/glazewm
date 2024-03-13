7805019c706f1ef2dd900567c59bfb4eb952fffb
Yuu528 <yuu528mail@gmail.com>
2024-03-13 13:49:18 +0900

    Add build script

---
+++ make.cmd
@@ -0,0 +1,1 @@
+dotnet publish ./GlazeWM.App/GlazeWM.App.csproj --configuration=Release --runtime=win-x64 --output=. --self-contained -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true
